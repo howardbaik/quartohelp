06908d014b76f6240b87d1157ac5d9b9edcaa0a1
Tomasz Kalinowski <kalinowskit@gmail.com>
2025-09-08 08:12:12 -0400

    add `configure_chat()`

---
+++ R/client.R
@@ -0,0 +1,43 @@
+configure_chat <- function(
+  chat = ellmer::chat_openai(
+    model = "gpt-5-nano",
+    params = ellmer::params(
+      reasoning_effort = "low",
+      verbosity = "low"
+    ),
+    echo = FALSE
+  ),
+  top_k = 8,
+  store = quartohelp_ragnar_store()
+) {
+  stopifnot(inherits(chat, "Chat"))
+  chat$set_system_prompt(c(
+    chat$get_system_prompt(),
+    glue::trim(
+      "
+    You are an expert in Quarto documentation. You are concise.
+    Always perform a search of the Quarto knowledge store for each user request.
+    Every response must include links to official documentation sources.
+    If the request is ambiguous, search first, then ask a clarifying question.
+    If docs are unavailable, or if search fails, or if docs do not contain an answer
+    to the question, inform the user and do NOT answer the question.
+
+    Always give answers that include a minimal fully self-contained quarto document.
+
+    To display Quarto code blocks, use oversized markdown fences, like this:
+
+    ````` markdown
+    PROSE HERE
+    ```{r}
+    CODE HERE
+    ```
+    ```{python}
+    CODE HERE
+    ```
+    `````
+    "
+    )
+  ))
+
+  ragnar::ragnar_register_tool_retrieve(chat, store, top_k = top_k)
+}
